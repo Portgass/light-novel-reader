fa7f9de801eb44d9389904f92dcc7646d4ad040a
Petr D. Svoboda <petrdsvoboda@gmail.com>
2015-09-29 16:13:52 +0200

    moved wordmaster to its folder

---
+++ novels/konjiki-no-wordmaster.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <title>Konjiki no Wordmaster</title>
+  <meta name="description" content="Konjiki no Wordmaster">
+  <meta name="author" content="Petr D. Svoboda">
+  <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+  <div class="container">
+
+    <h1 id="top">Konjiki no Wordmaster</h1>
+
+    <a class="act" href="#a1">Act 1</a>
+    <a href="#c38">Chapter 38: Arnold’s Master</a>
+    <a href="#c39">Chapter 39: The unstoppable war</a>
+    <a href="#c40">Chapter 40: War Commences! And a Sudden Development!?</a>
+    <a href="#c41">Chapter 41: Interruption of War</a>
+    <a href="#c42">Chapter 42: Another Conference in the Demon Country</a>
+    <a href="#c43">Chapter 43: Rarashik’s Ability</a>
+    <a href="#c44">Chapter 44: Muir’s Test Complete!</a>
+    <a href="#c45">Chapter 45: The Gabranth’s Roots</a>
+    <a href="#c46">Chapter 46: Hiiro's Decision</a>
+    <a href="#c47">Chapter 47: Arnold’s other objective</a>
+    <a href="#c48">Chapter 48: His Sister, the Maid</a>
+    <a href="#c49">Chapter 49: Hiiro and the girl</a>
+    <a href="#c50">Chapter 50: Mimiru and the boy</a>
+    <a href="#c51">Chapter 51: Promise Between the Two</a>
+    <a href="#c52">Chapter 52: Arnold and company’s Suspicions</a>
+    <a href="#c53">Chapter 53: Identity of the ghost and friends</a>
+    <a href="#c54">Chapter 54: A New Journey</a>
+    <a href="#c55">Chapter 55: That bird, again!</a>
+    <a href="#c56">Chapter 56: Movement in Victorias</a>
+    <a href="#c57">Chapter 57: One Month Later</a>
+    <a href="#c58">Chapter 58: Now, to the Evila Continent!</a>
+    <a href="#c59">Chapter 59: The Threat of the Grand Slime!</a>
+    <a href="#c60">Chapter 60: Resurrecting Butler?</a>
+    <a href="#c61">Chapter 61: Insightful Butler Silvan</a>
+    <a href="#c62">Chapter 62: A New Pervert (Companion)?</a>
+    <a href="#c63">Chapter 63: Venom Mountain</a>
+    <a href="#c64">Chapter 64: The fight with the Cactus Man</a>
+    <a href="#c65">Chapter 65: Towards the Mansion</a>
+    <a href="#c66">Chapter 66: The Chaotic Trio</a>
+    <a href="#c67">Chapter 67: The Devious Girl Liliyn</a>
+    <a href="#c68">Chapter 68: The Finesse of a Butler</a>
+
+    <a class="act" href="#a2">Act 2</a>
+
+    <h2 id="a1"><a href="#top">Act 1</a></h2>
+
+    <?php include 'html/chapters/c38.html'; ?>
+    <?php include 'html/chapters/c39.html'; ?>
+    <?php include 'html/chapters/c40.html'; ?>
+    <?php include 'html/chapters/c41.html'; ?>
+    <?php include 'html/chapters/c42.html'; ?>
+    <?php include 'html/chapters/c43.html'; ?>
+    <?php include 'html/chapters/c44.html'; ?>
+    <?php include 'html/chapters/c45.html'; ?>
+    <?php include 'html/chapters/c46.html'; ?>
+    <?php include 'html/chapters/c47.html'; ?>
+    <?php include 'html/chapters/c48.html'; ?>
+    <?php include 'html/chapters/c49.html'; ?>
+    <?php include 'html/chapters/c50.html'; ?>
+    <?php include 'html/chapters/c51.html'; ?>
+    <?php include 'html/chapters/c52.html'; ?>
+    <?php include 'html/chapters/c53.html'; ?>
+    <?php include 'html/chapters/c54.html'; ?>
+    <?php include 'html/chapters/c55.html'; ?>
+    <?php include 'html/chapters/c56.html'; ?>
+    <?php include 'html/chapters/c57.html'; ?>
+    <?php include 'html/chapters/c58.html'; ?>
+    <?php include 'html/chapters/c59.html'; ?>
+    <?php include 'html/chapters/c60.html'; ?>
+    <?php include 'html/chapters/c61.html'; ?>
+    <?php include 'html/chapters/c62.html'; ?>
+    <?php include 'html/chapters/c63.html'; ?>
+    <?php include 'html/chapters/c64.html'; ?>
+    <?php include 'html/chapters/c65.html'; ?>
+    <?php include 'html/chapters/c66.html'; ?>
+    <?php include 'html/chapters/c67.html'; ?>
+    <?php include 'html/chapters/c68.html'; ?>
+
+    <h2 id="a2"><a href="#top">Act 2</a></h2>
+
+  </div>
+
+</body>
+
+</html>
