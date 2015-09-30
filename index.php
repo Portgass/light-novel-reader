10f938f0d20ce6c75be4877c4e841f7d5059a176
Petr D. Svoboda <petrdsvoboda@gmail.com>
2015-09-30 13:00:36 +0200

    added landing page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 
   <meta charset="utf-8">
-  <title>Konjiki no Wordmaster</title>
+  <title>Light Novel Reader</title>
   <meta name="description" content="Konjiki no Wordmaster">
   <meta name="author" content="Petr D. Svoboda">
   <link rel="stylesheet" href="css/main.css">
@@ -13,11 +13,16 @@
 
 <body>
 
-  <div class="container">
+  <div id="landing-page">
 
-    <h1 id="top">Konjiki no Wordmaster</h1>
+    <h1 id="top">Light Novel Reader</h1>
 
-    
+      <ul id="novel-menu">
+        <li class="list-konjiki"><a href="novels/konjiki-no-wordmaster.php"></a><span>Konjiki no Wordmaster</span></li>
+        <li class="list-sculptor"><a href="novels/legendary-moonlight-sculptor.php"></a><span>Legendary Moonlight Sculptor</span></li>
+      <ul>
+
+
 
   </div>
 
